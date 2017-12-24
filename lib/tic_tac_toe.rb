469adc593941100b3422044ee5110ad06b3c6742
Stephane <son.trinh.dev@gmail.com>
2017-12-24 16:41:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,5 +3,25 @@ class TicTacToe
     @board = Array.new(9, ' ')
   end
   
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    # win by columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    # win by diangonal
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts '-' * 11
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts '-' * 11
+    puts " #{@board[6]} | #{@board[7} | #{@board[8] } "
+  end
+
 
 end
